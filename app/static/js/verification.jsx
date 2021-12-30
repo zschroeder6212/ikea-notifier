886db4dc716a50afdd5887439d8b96203a4c098c
Zachary Schroeder <zschroeder6212@gmail.com>
2021-12-30 15:41:33 -0600

    changed notification page style

--- app/static/js/verification.jsx
+++ app/static/js/verification.jsx
@@ -5,6 +5,11 @@ import './verification.css';
 import './normalize.css';
 
 ReactDOM.render(
-      <h1>A verification email has been sent. You must verify tour email before you are notified. Please allow up to 30 minutes for the email to be delivered.</h1>,
-      document.getElementById('react-entrypoint')
+    <div id="message-box">
+        <h2>Success!</h2>
+        <div id="inner-box">
+            <span class="notification">A verification email has been sent. You must verify your email before you are notified. Please allow up to 30 minutes for the email to be delivered.</span>
+        </div>
+    </div>,
+    document.getElementById('react-entrypoint')
 );
